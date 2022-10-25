af7574b6a7e8fc98775d0fb54abb80169dd06560
mdanys <mochammaddany@gmail.com>
2022-10-25 16:43:25 +0700

    changing func S3 and add ERD

--- features/product/delivery/request.go
+++ features/product/delivery/request.go
@@ -5,7 +5,7 @@ import (
 )
 
 type ProductFormat struct {
-	Image  string `json:"image" form:"image"`
+	Images string `json:"image" form:"image"`
 	Name   string `json:"name" form:"name"`
 	Price  uint   `json:"price" form:"price"`
 	Qty    uint   `json:"qty" form:"qty"`
@@ -17,7 +17,7 @@ func ToCore(i interface{}) product.Core {
 	switch i.(type) {
 	case ProductFormat:
 		cnv := i.(ProductFormat)
-		return product.Core{Image: cnv.Image, Name: cnv.Name, Price: cnv.Price, Qty: cnv.Qty, Detail: cnv.Detail, UserID: cnv.UserID}
+		return product.Core{Images: cnv.Images, Name: cnv.Name, Price: cnv.Price, Qty: cnv.Qty, Detail: cnv.Detail, UserID: cnv.UserID}
 	}
 	return product.Core{}
 }
--- features/product/repository/model.go
+++ features/product/repository/model.go
@@ -7,19 +7,13 @@ import (
 )
 
 type User struct {
-	gorm.Model
-	Name     string
-	Images   string
-	Phone    string
-	Email    string
-	Password string
-	Bio      string
-	Saldo    uint
+	ID   uint
+	Name string
 }
 
 type Products struct {
 	gorm.Model
-	Image  string
+	Images string
 	Name   string
 	Price  uint
 	Qty    uint
@@ -31,33 +25,35 @@ type Products struct {
 func FromCore(pc product.Core) Products {
 	return Products{
 		Model:  gorm.Model{ID: pc.ID, CreatedAt: pc.CreatedAt, UpdatedAt: pc.UpdatedAt},
-		Image:  pc.Image,
+		Images: pc.Images,
 		Name:   pc.Name,
 		Price:  pc.Price,
 		Qty:    pc.Qty,
 		Detail: pc.Detail,
 		UserID: pc.UserID,
-		User:   User{Name: pc.Name},
+		User:   User{ID: pc.ID, Name: pc.Name},
 	}
 }
 
 func ToCore(p Products) product.Core {
 	return product.Core{
-		ID:     p.ID,
-		Image:  p.Image,
-		Name:   p.Name,
-		Price:  p.Price,
-		Qty:    p.Qty,
-		Detail: p.Detail,
-		UserID: p.UserID,
-		User:   product.User{Name: p.Name},
+		ID:        p.ID,
+		Images:    p.Images,
+		Name:      p.Name,
+		Price:     p.Price,
+		Qty:       p.Qty,
+		Detail:    p.Detail,
+		UserID:    p.UserID,
+		User:      product.User{ID: p.ID, Name: p.Name},
+		CreatedAt: p.CreatedAt,
+		UpdatedAt: p.UpdatedAt,
 	}
 }
 
 func ToCoreArray(pa []Products) []product.Core {
 	var res []product.Core
 	for _, val := range pa {
-		res = append(res, product.Core{ID: val.ID, Image: val.Image, Name: val.Name, Price: val.Price, Qty: val.Qty, Detail: val.Detail, UserID: val.UserID, User: product.User{Name: val.Name}})
+		res = append(res, product.Core{ID: val.ID, Images: val.Images, Name: val.Name, Price: val.Price, Qty: val.Qty, Detail: val.Detail, UserID: val.UserID, User: product.User{ID: val.ID, Name: val.Name}})
 	}
 	return res
 }
